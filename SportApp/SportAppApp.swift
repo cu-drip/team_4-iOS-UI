fbaf627c9dcf663a5fe0e5ecd7c2bea8b7b26a6f
kituNew <vlad640vlad@gmail.com>
2025-07-01 18:32:47 +0300

    Initial Commit

---
+++ SportApp/SportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportAppApp.swift
+//  SportApp
+//
+//  Created by Zaitsev Vladislav on 01.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportAppTests/SportAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportAppTests.swift
+//  SportAppTests
+//
+//  Created by Zaitsev Vladislav on 01.07.2025.
+//
+
+import Testing
+@testable import SportApp
+
+struct SportAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
